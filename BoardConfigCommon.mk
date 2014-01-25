df4f9fc7282efa3ee19175b73686e5f2c040a755
Vojtech Bocek <vbocek@gmail.com>
2014-01-25 21:09:00 +0100

    Add Ubuntu Touch rom_info.txt

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,6 +132,7 @@ MR_RD_ADDR := 0x82500000
 MR_DPI := xhdpi
 MR_FSTAB := device/asus/flo/twrp.fstab
 MR_KEXEC_MEM_MIN := 0x85000000
+MR_INFOS := device/asus/flo/mrom_infos
 
 BUILD_OLD_SYS_PROPS := true
 COMMON_GLOBAL_CFLAGS += -DBUILD_OLD_SYS_PROPS
