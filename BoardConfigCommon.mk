e0ce1ed0fbc253ce16faf0d618affc1aad146260
Vojtech Bocek <vbocek@gmail.com>
2014-08-03 18:46:00 +0200

    Add MR_DPI_FONT define to BoardCondfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,6 +129,7 @@ MR_INPUT_TYPE := type_b
 MR_INIT_DEVICES := device/asus/flo/mr_init_devices.c
 MR_RD_ADDR := 0x82500000
 MR_DPI := xhdpi
+MR_DPI_FONT := 323
 MR_FSTAB := device/asus/flo/twrp.fstab
 MR_KEXEC_MEM_MIN := 0x85000000
 MR_INFOS := device/asus/flo/mrom_infos
